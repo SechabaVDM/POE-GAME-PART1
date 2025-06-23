8704a2580cf6c00d58c9065f1c7d8906ff4fa8d2
xil555 <st10438975@vgconnect.edu.za>
2025-06-23 23:37:22 +0200

    g

--- Assets/Scripts/CollectMindFragment.cs
+++ Assets/Scripts/CollectMindFragment.cs
@@ -4,6 +4,14 @@ public class CollectMindFragment : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
+<<<<<<< Updated upstream
+=======
+        if (other.CompareTag("Mind"))
+        {
+            audioSource.PlayOneShot(hitSound);
+        }
+       
+>>>>>>> Stashed changes
         MindFragments.mindCount += 1;
         Destroy(gameObject);
     }
